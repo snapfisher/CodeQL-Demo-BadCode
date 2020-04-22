2fb7d826b2847e45d60a8d06b9c98dee0fea431c
Paul Fisher <pfisher@microsoft.com>
2020-04-22 10:26:23 -0400

    update

---
+++ CodeQL-Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeQL_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool error = false;
+
+            if (error) { }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
